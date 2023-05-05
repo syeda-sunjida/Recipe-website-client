3ca62e18fbc6100c3b6ce850d68f368b27a6f0d1
syeda-sunjida <sunjidaferdous123@gmail.com>
2023-05-05 19:24:49 +0600

    Layout complete

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import Login from '../Components/Login';
-import Blog from '../Components/Blog';
-import Register from '../Components/Register';
+
 import ExtraOne from '../Components/ExtraOne';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            {/* <Login></Login>
-            <Register></Register>
-            <Blog></Blog> */}
+           
             <Outlet></Outlet>
             <ExtraOne></ExtraOne>
             <Footer></Footer>
