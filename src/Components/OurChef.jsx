227a42deb8af4e4992d89c9fe6c67c79dc2c618e
syeda-sunjida <sunjidaferdous123@gmail.com>
2023-05-05 19:28:06 +0600

    chefs part done

--- src/Components/OurChef.jsx
+++ src/Components/OurChef.jsx
@@ -11,7 +11,7 @@ const OurChef = () => {
   return (
     <div className="container mx-auto">
       <h2 className="md:text-6xl text-2xl my-10 text-center font-bold">
-        Our Chefs
+        Amader Radhunira
       </h2>
 
       <div className="container md:grid grid-cols-3  gap-10 md:my-20">
